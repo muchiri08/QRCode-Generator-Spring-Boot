6d944b85f651e0b06d7a153b2dbf366687444e87
muchiri08 <mbogokennedy08@gmail.com>
2023-05-06 16:34:49 +0300

    called method QRCodeGenerator inside getAllProducts method

--- src/main/java/com/muchiri/qrcodegenerator/service/ProductService.java
+++ src/main/java/com/muchiri/qrcodegenerator/service/ProductService.java
@@ -1,10 +1,13 @@
 package com.muchiri.qrcodegenerator.service;
 
+import com.google.zxing.WriterException;
 import com.muchiri.qrcodegenerator.entity.Product;
 import com.muchiri.qrcodegenerator.repository.ProductRepository;
+import com.muchiri.qrcodegenerator.util.QRCodeGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -22,6 +25,19 @@ public class ProductService {
 
     public List<Product> getAllProducts(){
         List<Product> products = productRepository.findAll();
+
+        //Generate and store barcode for each product
+        if(!products.isEmpty()){
+            products.forEach(product -> {
+                try {
+                    QRCodeGenerator.GenerateQRCode(product);
+                } catch (WriterException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
 
         return products;
     }
